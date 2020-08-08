371ccb297a98da5809e7d2b38abf1356ce9b1f53
glruff20 <69280604+glruff20@users.noreply.github.com>
2020-08-08 15:33:33 -0400

    updates

--- train_data/hackathon_code.R
+++ train_data/hackathon_code.R
@@ -129,10 +129,10 @@ summary(fit_nasal_gene)
 #now have regression models for each data table; use these to predict severity scores for each patient in test data
 
 #download test data tables
-test_cd19=read.table("cd19_gene_expr_train.txt", check.names=F)
-test_cd4=read.table("cd4_gene_expr_train.txt", check.names=F)
-test_cd8=read.table("cd8_gene_expr_train.txt", check.names=F)
-test_nasal_gene=read.table("nasal_gene_expr_train.txt", check.names=F)
+test_cd19=read.table("cd19_gene_expr_test.txt", check.names=F)
+test_cd4=read.table("cd4_gene_expr_test.txt", check.names=F)
+test_cd8=read.table("cd8_gene_expr_test.txt", check.names=F)
+test_nasal_gene=read.table("nasal_gene_expr_test.txt", check.names=F)
 
 #subset out useful columns for each data file
 test_cd19=t(test_cd19)
@@ -164,26 +164,19 @@ cd4_predict=predict(fit_cd4, data.frame(test_cd4_features))
 cd8_predict=predict(fit_cd8, data.frame(test_cd8_features))
 nasal_gene_predict=predict(fit_nasal_gene, data.frame(test_nasal_gene_features))
 
-head(cd19_predict)
-cd4_cd19=rbind(cd4_predict, cd19_predict)
-cd4_cd19
-cd4_predict
-cd19_predict
-
+#combine predictions into table
 write.table(cd4_predict, file="cd4_predictions.txt")
 write.table(cd8_predict, file="cd8_predictions.txt")
 write.table(cd19_predict, file="cd19_predictions.txt")
 write.table(nasal_gene_predict, file="nasal_gene_predictions.txt")
 
 cd4_predict=read.table("cd4_predictions.txt")
-head(test)
 cd8_predict=read.table("cd8_predictions.txt")
 cd19_predict=read.table("cd19_predictions.txt")
 nasal_gene_predict=read.table("nasal_gene_predictions.txt")
 
 
 cd4_cd8=merge(cd4_predict, cd8_predict, by=0, all.x=T, all.y=T)
-head(cd4_cd8)
 rownames(cd4_cd8)=cd4_cd8$Row.names
 cd4_cd8=cd4_cd8[,-c(1)]
 cd4_8_19=merge(cd4_cd8, cd19_predict, by=0, all.x=T, all.y=T)
@@ -198,3 +191,49 @@ predict_average=rowMeans(merged_predict, na.rm=T)
 test=cbind.data.frame(merged_predict, predict_average)
 merged_predict=test
 head(merged_predict)
+
+#match numbers up with their order for prediction.csv file
+
+prediction=read.table("prediction.csv", sep=",")
+head(prediction)
+prediction=prediction[-c(1),]
+rownames(prediction)=prediction$V2
+test=merge(prediction, merged_predict, by=0, all.x=T, all.y=T, sort=F)
+test
+
+test2=cbind.data.frame(test$predict_average, test$Row.names)
+head(test2)
+
+write.table(test2, file="my_predictions.csv", sep=",") #first draft, problems below
+
+
+#issues with current predictions - nasal gene large variance
+#3 patients dont have any predictions (no data) - at bottom of file right now
+#figure out how to make nasal gene predictions better - just use median?
+#how to use nasal microbio dataset??
+
+
+histogram(test$predict_average)
+histogram(test$cd4)
+mean(test$nasal_gene, na.rm=T)
+median(test$nasal_gene, na.rm=T)
+test$nasal_gene
+
+#first attempt to fix - just use median for nasal gene predictions (only for patients that have nasal gene data)
+nasal_predict_median=cbind.data.frame(nasal_gene_predict, rep(median(test$nasal_gene, na.rm=T), 42))
+
+merged_predict2=merge(cd4_8_19, nasal_predict_median, by=0, all.x=T, all.y=T)
+rownames(merged_predict2)=merged_predict2$Row.names
+head(merged_predict2)
+merged_predict2=merged_predict2[,-c(1,5)]
+colnames(merged_predict2)=c("cd4", "cd8", "cd19", "nasal_gene_median")
+predict_average2=rowMeans(merged_predict2, na.rm=T)
+merged_predict2=cbind.data.frame(merged_predict2, predict_average2)
+
+test=merge(prediction, merged_predict2, by=0, all.x=T, all.y=T, sort=F)
+
+test2=cbind.data.frame(test$predict_average, test$Row.names)
+head(test2)
+
+write.table(test2, file="my_predictions2.csv", sep=",") #use median of nasal_gene table, still missing 3 patients severity scores
+
