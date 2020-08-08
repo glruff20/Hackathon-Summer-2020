c5ce3aeb96fb0805fe95cab862292fc11d4d6675
glruff20 <69280604+glruff20@users.noreply.github.com>
2020-08-08 13:34:14 -0400

    Update hackathon_code.R

--- train_data/hackathon_code.R
+++ train_data/hackathon_code.R
@@ -52,3 +52,95 @@ rownames(severe_cd19)=severe_cd19[,1]
 severe_cd19=severe_cd19[,-c(1,3)]
 colnames(severe_cd19)=c("severity_score", colnames(severe_cd19[,2:5828]))
 
+#calculate correlation scores for each gene vs severity score; take top portion
+
+#make severity score column numeric for all data tables
+severe_cd19[,1]=as.numeric(paste(severe_cd19[,1]))
+severe_cd4[,1]=as.numeric(paste(severe_cd4[,1]))
+severe_cd8[,1]=as.numeric(paste(severe_cd8[,1]))
+severe_nasal_gene[,1]=as.numeric(paste(severe_nasal_gene[,1]))
+severe_nasal_micro[,1]=as.numeric(paste(severe_nasal_micro[,1]))
+
+#calculate cors for each gene in gene expr tables, subset out top 12 or so
+cor_severe_cd19=cor(severe_cd19, use="pairwise.complete.obs")
+summary(cor_severe_cd19[,1])
+sig_cor_severe_cd19=subset(cor_severe_cd19, cor_severe_cd19[1,] < -0.43 | cor_severe_cd19[1,] > 0.43)
+dim(sig_cor_severe_cd19)
+rownames(sig_cor_severe_cd19)
+sig_cor_severe_cd19[,1]
+
+cor_severe_cd8=cor(severe_cd8, use="pairwise.complete.obs")
+summary(cor_severe_cd8[,1])
+sig_cor_severe_cd8=subset(cor_severe_cd8, cor_severe_cd8[1,] < -0.55 | cor_severe_cd8[1,] > 0.6)
+dim(sig_cor_severe_cd8)
+rownames(sig_cor_severe_cd8)
+sig_cor_severe_cd8[,1]
+
+cor_severe_cd4=cor(severe_cd4, use="pairwise.complete.obs")
+summary(cor_severe_cd4[,1])
+sig_cor_severe_cd4=subset(cor_severe_cd4, cor_severe_cd4[1,] < -0.41 | cor_severe_cd4[1,] > 0.45)
+sig_cor_severe_cd4=subset(cor_severe_cd4, cor_severe_cd4[1,] < -0.35 | cor_severe_cd4[1,] > 0.4)
+
+dim(sig_cor_severe_cd4)
+rownames(sig_cor_severe_cd4)
+sig_cor_severe_cd4[,1]
+
+cor_severe_nasal_gene=cor(severe_nasal_gene, use="pairwise.complete.obs")
+summary(cor_severe_nasal_gene[,1])
+sig_cor_severe_nasal_gene=subset(cor_severe_nasal_gene, cor_severe_nasal_gene[1,] < -0.40 | cor_severe_nasal_gene[1,] > 0.40)
+dim(sig_cor_severe_nasal_gene)
+rownames(sig_cor_severe_nasal_gene)
+sig_cor_severe_nasal_gene[,1]
+
+
+
+#figure out how to correlate nasal micro data??
+cor_severe_nasal_micro=cor(severe_nasal_micro, use="pairwise.complete.obs")
+summary(cor_severe_nasal_gene[,1])
+sig_cor_severe_nasal_gene=subset(cor_severe_nasal_gene, cor_severe_nasal_gene[1,] < -0.43 | cor_severe_nasal_gene[1,] > 0.45)
+dim(sig_cor_severe_nasal_gene)
+rownames(sig_cor_severe_nasal_gene)
+sig_cor_severe_nasal_gene[,1]
+
+
+#now that have features chosen, subset out tables to have severity as col1
+#all significant features as the rest of the columns
+
+cd4_features=subset(severe_cd4, select=rownames(sig_cor_severe_cd4))
+cd8_features=subset(severe_cd8, select=rownames(sig_cor_severe_cd8))
+sig_cor_severe_cd19=sig_cor_severe_cd19[1:28,] #15th row was NA, not sure why, deleted it
+cd19_features=subset(severe_cd19, select=rownames(sig_cor_severe_cd19))
+nasal_gene_features=subset(severe_nasal_gene, select=rownames(sig_cor_severe_nasal_gene))
+
+
+#create a linear regression model from those data tables for each dataset
+library(DAAG) #use for cross validation testing
+
+fit_cd4=lm(cd4_features$severity_score ~., data=cd4_features)
+summary(fit_cd4)
+
+fit_cd8=lm(cd8_features$severity_score ~., data=cd8_features)
+summary(fit_cd8)
+
+fit_cd19=lm(cd19_features$severity_score ~., data=cd19_features)
+summary(fit_cd19)
+
+fit_nasal_gene=lm(nasal_gene_features$severity_score ~., data=nasal_gene_features)
+summary(fit_nasal_gene)
+
+#now have regression models for each data table; use these to predict severity scores for each patient in test data
+
+#download test data tables
+test_cd19=read.table("cd19_gene_expr_train.txt", check.names=F)
+test_cd4=read.table("cd4_gene_expr_train.txt", check.names=F)
+test_cd8=read.table("cd8_gene_expr_train.txt", check.names=F)
+test_nasal_gene=read.table("nasal_gene_expr_train.txt", check.names=F)
+
+#subest out useful columns for each data file
+head(colnames(test_cd19))
+test_cd19=t(test_cd19)
+test_cd4=t(test_cd4)
+test_cd8=t(test_cd8)
+test_nasal_gene=t(test_nasal_gene)
+
+test_cd19_features=subset(test_cd19, select=rownames(sig_cor_severe_cd19))
